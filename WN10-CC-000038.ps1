9bcb0651617551c62d297de76123acdf1c6aced4
jameslittle05 <james.b.little@outlook.com>
2025-02-14 13:00:37 -0500

    Create WN10-CC-000038.ps1

---
+++ WN10-CC-000038.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that WDigest Authentication must be disabled.
+
+.NOTES
+    Author          : James Little
+    LinkedIn        : 
+    GitHub          : github.com/jameslittle05
+    Date Created    : 2025-02-14
+    Last Modified   : 2025-02-14
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000038
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000038).ps1 
+#>
+
+
+# Define the registry path and the value name
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\System"
+$valueName = "MaxSize"
+$valueData = 0x00008000  # 32768 in hexadecimal
+
+# Check if the registry key exists, if not, create it
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force
+}
+
+# Set the registry value to the desired value (MaxSize = 32768)
+Set-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -Type DWord
+
+Write-Host "Registry value set successfully."
