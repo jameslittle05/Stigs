3298a0ef812383a063cfcb8ed40c8f4dd5635ed1
jameslittle05 <james.b.little@outlook.com>
2025-02-14 12:55:28 -0500

    Update WN10-AU-000510.ps1

--- WN10-AU-000510.ps1
+++ WN10-AU-000510.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures that the The System event log size must be configured to 32768 KB or greater.
 
 .NOTES
     Author          : James Little
